7d4ba99a39c5b2d503b0b561e573308fabacb181
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-04-29 18:19:15 -0400

    add AppState container class

---
+++ src/AppState.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+export interface Props {
+    children: any,
+}
+
+export interface State {
+
+}
+
+
+/*
+    Container for App's State, exports functions to every child
+    component that allow the augmentation and fetching of the App's top level State
+ */
+class AppState extends React.Component<Props, State> {
+    constructor(props: Props) {
+       super(props);
+       this.state = {
+
+       };
+    }
+
+    setAppState(newState: State) {
+        this.setState(newState);
+    }
+
+    getAppState() {
+        return this.state;
+    }
+
+    render() {
+        return (
+            <div className="AppState">
+                {React.Children.map(this.props.children, child => {
+                    return React.cloneElement(child, {
+                        appState: this.getAppState,
+                        setAppState: this.setAppState
+                    });
+                })}
+            </div>
+        )
+    }
+}
+
+export default AppState;
